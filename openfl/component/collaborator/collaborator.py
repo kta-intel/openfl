ffe7b47d60c2c4cd9781b0db5067397e7fd5544c
kta-intel <kevin.ta@intel.com>
2024-12-16 14:31:57 -0800

    add todo
    Signed-off-by: kta-intel <kevin.ta@intel.com>

--- openfl/component/collaborator/collaborator.py
+++ openfl/component/collaborator/collaborator.py
@@ -163,9 +163,6 @@ class Collaborator:
         """Run the collaborator."""
         memory_details = []
         while True:
-            # if self.fim:
-            #     self.start_flower_client_connection()
-            # else:
             tasks, round_number, sleep_time, time_to_quit = self.get_tasks()
             if time_to_quit:
                 break
@@ -254,6 +251,7 @@ class Collaborator:
             func_name = self.task_config[task_name]["function"]
             kwargs = self.task_config[task_name]["kwargs"]
         if func_name=="start_client_adapter":
+            # TODO Need a major elegant and robust way to implement this
             if hasattr(self.task_runner, func_name):
                 method = getattr(self.task_runner, func_name)
                 if callable(method):
