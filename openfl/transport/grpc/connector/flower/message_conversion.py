21c13b5ca10ab480af617480e1fbde3ee58d9047
kta-intel <kevin.ta@intel.com>
2025-01-03 14:03:00 -0800

    more docstring
    Signed-off-by: kta-intel <kevin.ta@intel.com>

--- openfl/transport/grpc/connector/flower/message_conversion.py
+++ openfl/transport/grpc/connector/flower/message_conversion.py
@@ -1,22 +1,36 @@
 from flwr.proto import grpcadapter_pb2
-from openfl.protocols  import aggregator_pb2
+from openfl.protocols import aggregator_pb2
 # from openfl.transport.grpc.connector.flower.deserialize_message import deserialize_flower_message
 
 def flower_to_openfl_message(flower_message, header=None):
-    """Convert a Flower MessageContainer to an OpenFL OpenFLMessage."""
-    if isinstance(flower_message, aggregator_pb2.DropPod()):
+    """
+    Convert a Flower MessageContainer to an OpenFL DropPod.
+
+    This function takes a Flower MessageContainer and converts it into an OpenFL DropPod.
+    If the input is already an OpenFL DropPod, it returns the input as-is.
+
+    Args:
+        flower_message (grpcadapter_pb2.MessageContainer or aggregator_pb2.DropPod): 
+            The Flower message to be converted. It can either be a Flower MessageContainer 
+            or an OpenFL DropPod.
+        header (aggregator_pb2.MessageHeader, optional): 
+            An optional header to be included in the OpenFL DropPod. If provided, 
+            it will be copied to the DropPod's header field.
+
+    Returns:
+        aggregator_pb2.DropPod: The converted OpenFL DropPod message.
+    """
+    if isinstance(flower_message, aggregator_pb2.DropPod):
         # If the input is already an OpenFL message, return it as-is
         return flower_message
     else:
-        """Convert a Flower MessageContainer to an OpenFL message."""
         # Create the OpenFL message
         openfl_message = aggregator_pb2.DropPod()
         # Set the MessageHeader fields based on the provided sender and receiver
         if header:
             openfl_message.header.CopyFrom(header)
-        # openfl_message.message_type = flower_message.metadata['grpc-message-qualname']
-
-        # TODO: Add verification steps for messages coming from Flower entities
+        
+        # Serialize the Flower message and set it in the OpenFL message
         serialized_flower_message = flower_message.SerializeToString()
         openfl_message.message.npbytes = serialized_flower_message
         openfl_message.message.size = len(serialized_flower_message)
@@ -24,12 +38,25 @@ def flower_to_openfl_message(flower_message, header=None):
         return openfl_message
 
 def openfl_to_flower_message(openfl_message):
-    """Convert an OpenFL OpenFLMessage to a Flower MessageContainer."""
+    """
+    Convert an OpenFL DropPod to a Flower MessageContainer.
+
+    This function takes an OpenFL DropPod and converts it into a Flower MessageContainer.
+    If the input is already a Flower MessageContainer, it returns the input as-is.
+
+    Args:
+        openfl_message (aggregator_pb2.DropPod or grpcadapter_pb2.MessageContainer): 
+            The OpenFL message to be converted. It can either be an OpenFL DropPod 
+            or a Flower MessageContainer.
+
+    Returns:
+        grpcadapter_pb2.MessageContainer: The converted Flower MessageContainer.
+    """
     if isinstance(openfl_message, grpcadapter_pb2.MessageContainer):
         # If the input is already a Flower message, return it as-is
         return openfl_message
     else:
-    # Deserialize the Flower message from the DataStream npbytes field
+        # Deserialize the Flower message from the DataStream npbytes field
         flower_message = grpcadapter_pb2.MessageContainer()
         flower_message.ParseFromString(openfl_message.message.npbytes)
         return flower_message
