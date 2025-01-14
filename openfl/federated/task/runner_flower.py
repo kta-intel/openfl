b91e8871aabd47d731396a35b1902183afd6d13c
kta-intel <kevin.ta@intel.com>
2025-01-14 08:53:50 -0800

    adding try excepts for subprocess shutdown
    Signed-off-by: kta-intel <kevin.ta@intel.com>

--- openfl/federated/task/runner_flower.py
+++ openfl/federated/task/runner_flower.py
@@ -13,108 +13,57 @@ import time
 
 
 class FlowerTaskRunner(TaskRunner):
-    """
-    FlowerTaskRunner is a task runner that executes Flower SuperNode
-    to initialize the experiment from the client side.
-
-    This class is responsible for starting a local gRPC server and a Flower SuperNode
-    in a subprocess. It also provides options for automatic shutdown based on subprocess
-    activity.
-
-    Shutdown Options:
-    - Manual Shutdown: The server and supernode process can be manually stopped by pressing CTRL+C.
-    - Automatic Shutdown: If enabled, the system will monitor the activity of subprocesses and 
-      automatically shut down if no new subprocess starts within a certain time frame.
-    """
-
     def __init__(self, auto_shutdown=True, **kwargs):
-        """
-        Initializes the FlowerTaskRunner.
-
-        Args:
-            auto_shutdown (bool): Whether to enable automatic shutdown based on subprocess activity.
-                Default is True. Set to False for long-lived components.
-            **kwargs: Additional parameters to pass to the functions.
-        """
         super().__init__(**kwargs)
         self.logger = getLogger(__name__)
         self.num_partitions = self.data_loader.get_node_configs()[0]
         self.partition_id = self.data_loader.get_node_configs()[1]
 
-        # Define a base port number
         base_port = 5000
-
-        # Calculate the client port by adding the partition ID to the base port
         self.client_port = base_port + self.partition_id
         self.auto_shutdown = auto_shutdown
 
     def start_client_adapter(self, openfl_client, collaborator_name, **kwargs):
-        """
-        Starts the local gRPC server and the Flower SuperNode.
-
-        Args:
-            openfl_client: The OpenFL client instance used to communicate with the OpenFL server.
-            collaborator_name: The name of the collaborator.
-            **kwargs: Additional parameters, including 'local_server_port'.
-
-        The method performs the following steps:
-        1. Starts a local gRPC server to handle communication between the OpenFL client and the Flower SuperNode.
-        2. Launches the Flower SuperNode in a subprocess.
-        3. Sets up signal handlers for manual shutdown (via CTRL+C).
-        4. If auto_shutdown is enabled, monitors subprocess activity and initiates shutdown if no new subprocess starts within the expected time frame.
-
-        Shutdown Process:
-        - When a shutdown signal (SIGINT or SIGTERM) is received, the method will:
-            1. Terminate all child processes of the supernode subprocess.
-            2. Terminate the main supernode subprocess.
-            3. Stop the gRPC server.
-            4. Log the shutdown process and set the termination event to stop the server.
-        """
         local_server_port = kwargs['local_server_port']
 
-        # Start the local gRPC server
         server = grpc.server(ThreadPoolExecutor(max_workers=cpu_count()))
         grpcadapter_pb2_grpc.add_GrpcAdapterServicer_to_server(LocalGRPCServer(openfl_client, collaborator_name), server)
-        
-        # TODO: add restrictions
         server.add_insecure_port(f'[::]:{local_server_port}')
         server.start()
         self.logger.info(f"OpenFL local gRPC server started, listening on port {local_server_port}.")
 
-        # Start the Flower SuperNode in a subprocess
         command = [
             "flower-supernode",
             "--insecure",
             "--grpc-adapter",
-            "--superlink", f"127.0.0.1:{local_server_port}", #  note [kta-intel]: this connects to local gRPC server
+            "--superlink", f"127.0.0.1:{local_server_port}",
             "--clientappio-api-address", f"127.0.0.1:{self.client_port}",
             "--node-config", f"num-partitions={self.num_partitions} partition-id={self.partition_id}"
         ]
-        # Start the subprocess
         supernode_process = subprocess.Popen(command, shell=False)
 
-        # Create an event to wait for the termination signal
         termination_event = threading.Event()
 
         def signal_handler(_sig, _frame):
             self.logger.info("Received shutdown signal. Terminating supernode process...")
 
             if supernode_process.poll() is None:
-                # find and terminate client_app_process processes
-                main_subprocess = psutil.Process(supernode_process.pid)
-                client_app_processes = main_subprocess.children(recursive=True)
-                for client_app_process in client_app_processes:
-                    client_app_process.terminate()
-                _, still_alive = psutil.wait_procs(client_app_processes, timeout=1)
-                for p in still_alive:
-                    p.kill()
-                # Terminate the main process
-                supernode_process.terminate()
                 try:
-                    supernode_process.wait(timeout=1)
-                except subprocess.TimeoutExpired:
-                    supernode_process.kill()
-                self.logger.info("Supernode process terminated.")
+                    main_subprocess = psutil.Process(supernode_process.pid)
+                    client_app_processes = main_subprocess.children(recursive=True)
+                    for client_app_process in client_app_processes:
+                        client_app_process.terminate()
+                    _, still_alive = psutil.wait_procs(client_app_processes, timeout=1)
+                    for p in still_alive:
+                        p.kill()
+                    supernode_process.terminate()
+                    try:
+                        supernode_process.wait(timeout=1)
+                    except subprocess.TimeoutExpired:
+                        supernode_process.kill()
+                    self.logger.info("Supernode process terminated.")
+                except psutil.NoSuchProcess:
+                    self.logger.info("Supernode process already terminated.")
             else:
                 self.logger.info("Supernode process already terminated.")
 
@@ -132,7 +81,11 @@ class FlowerTaskRunner(TaskRunner):
             self.logger.info("Automatic shutdown enabled. Monitoring subprocess activity...")
 
             def monitor_subprocesses():
-                main_subprocess = psutil.Process(supernode_process.pid)
+                try:
+                    main_subprocess = psutil.Process(supernode_process.pid)
+                except psutil.NoSuchProcess:
+                    return
+
                 previous_end_time = None
                 intervals = []
 
@@ -145,14 +98,12 @@ class FlowerTaskRunner(TaskRunner):
                             if previous_end_time is not None:
                                 interval = end_time - previous_end_time
                                 intervals.append(interval)
-                                # self.logger.info(f"Subprocess ended. Interval: {interval:.2f} seconds.")
                             previous_end_time = end_time
 
                     if previous_end_time is not None:
                         running_timer = time.time() - previous_end_time
                         if intervals:
                             average_interval = sum(intervals) / len(intervals)
-                            # self.logger.info(f"Running timer: {running_timer:.2f} seconds. Average interval: {average_interval:.2f} seconds.")
                             if running_timer > 2 * average_interval:
                                 self.logger.info("No new subprocess started within the expected time. Initiating shutdown...")
                                 signal_handler(signal.SIGTERM, None)
@@ -167,5 +118,5 @@ class FlowerTaskRunner(TaskRunner):
         
         termination_event.wait()
 
-        if monitor_thread is not None:  # Ensure the monitor thread is properly terminated
+        if monitor_thread is not None:
             monitor_thread.join()
