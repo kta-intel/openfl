3d750ef538ce22e8bf57dd143861ee0b8d4edb0e
kta-intel <kevin.ta@intel.com>
2025-01-23 11:05:53 -0800

    add exceptions for graceful shutdown to forcefully terminate
    Signed-off-by: kta-intel <kevin.ta@intel.com>

--- openfl/component/interoperability/connector.py
+++ openfl/component/interoperability/connector.py
@@ -42,24 +42,30 @@ class Connector:
         Stop the server process if it is running.
         """
         if self._process:
-            self.logger.info(f"[OpenFL Connector] Stopping server process with PID: {self._process.pid}...")
-            # find and terminate sub_process processes
-            main_process = psutil.Process(self._process.pid)
-            sub_processes = main_process.children(recursive=True)
-            for sub_process in sub_processes:
-                self.logger.info(f"[OpenFL Connector] Stopping server subprocess with PID: {sub_process.pid}...")
-                sub_process.terminate()
-            _, still_alive = psutil.wait_procs(sub_processes, timeout=1)
-            for p in still_alive:
-                p.kill()
-            # Terminate the main process
-            self._process.terminate()
             try:
-                self._process.wait(timeout=1)
-            except subprocess.TimeoutExpired:
+                self.logger.info(f"[OpenFL Connector] Stopping server process with PID: {self._process.pid}...")
+                # find and terminate sub_process processes
+                main_process = psutil.Process(self._process.pid)
+                sub_processes = main_process.children(recursive=True)
+                for sub_process in sub_processes:
+                    self.logger.info(f"[OpenFL Connector] Stopping server subprocess with PID: {sub_process.pid}...")
+                    sub_process.terminate()
+                _, still_alive = psutil.wait_procs(sub_processes, timeout=1)
+                for p in still_alive:
+                    p.kill()
+                # Terminate the main process
+                self._process.terminate()
+                try:
+                    self._process.wait(timeout=1)
+                except subprocess.TimeoutExpired:
+                    self._process.kill()
+                self._process = None
+                self.logger.info("[OpenFL Connector] Server process stopped.")
+            except Exception as e:
+                self.logger.error(f"Error during graceful shutdown: {e}")
+                self.logger.info("Attempting forceful termination of superlink process...")
                 self._process.kill()
-            self._process = None
-            self.logger.info("[OpenFL Connector] Server process stopped.")
+                self.logger.info("Superlink process forcefully terminated.")
         else:
             self.logger.info("[OpenFL Connector] No server process is currently running.")
 
